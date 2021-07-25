01eb600cafc07362f9092dccc1853d71583c62a1
Raphael Queiroz <uroboros.phael@gmail.com>
2021-07-24 22:54:00 -0300

    feat: add user constructor.

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -3,7 +3,19 @@ defmodule Flightex.Users.User do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(_name, _email, cpf) when not is_bitstring(cpf) do
+    {:error, "Cpf must be a String"}
+  end
+
+  def build(name, email, cpf) do
+    {
+      :ok,
+      %__MODULE__{
+        name: name,
+        email: email,
+        cpf: cpf,
+        id: UUID.uuid1()
+      }
+    }
   end
 end
