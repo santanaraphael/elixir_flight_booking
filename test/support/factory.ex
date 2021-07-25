e07815dfaaa8a95d067fcd1238d1d1934d4ddc21
Raphael Queiroz <uroboros.phael@gmail.com>
2021-07-24 23:03:15 -0300

    feat: add input factories.

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -4,12 +4,29 @@ defmodule Flightex.Factory do
   alias Flightex.Bookings.Booking
   alias Flightex.Users.User
 
-  def users_factory do
+  def user_input_factory do
+    %{
+      name: "Jp",
+      email: "jp@banana.com",
+      cpf: "12345678900"
+    }
+  end
+
+  def user_factory do
     %User{
       id: UUID.uuid4(),
       name: "Jp",
       email: "jp@banana.com",
       cpf: "12345678900"
+    }
+  end
+
+  def booking_input_factory do
+    %{
+      complete_date: ~N[2001-05-07 03:05:00],
+      local_origin: "Brasilia",
+      local_destination: "Bananeiras",
+      user_id: "12345678900"
     }
   end
 
