45aabcbbc2c8f9ee1a753f9aafab99f21376073f
Raphael Queiroz <uroboros.phael@gmail.com>
2021-07-24 23:02:55 -0300

    feat: receive booking params in a map.

--- test/bookings/booking_test.exs
+++ test/bookings/booking_test.exs
@@ -3,23 +3,15 @@ defmodule Flightex.Bookings.BookingTest do
 
   alias Flightex.Bookings.Booking
 
+  import Flightex.Factory
+
   describe "build/4" do
     test "when all params are valid, returns a booking" do
-      {:ok, response} =
-        Booking.build(
-          ~N[2001-05-07 01:46:20],
-          "Brasilia",
-          "ilha das bananas",
-          "12345678900"
-        )
+      booking_input = build(:booking_input)
 
-      expected_response = %Flightex.Bookings.Booking{
-        complete_date: ~N[2001-05-07 01:46:20],
-        id: response.id,
-        local_destination: "ilha das bananas",
-        local_origin: "Brasilia",
-        user_id: "12345678900"
-      }
+      {:ok, response} = Booking.build(booking_input)
+
+      expected_response = build(:booking, id: response.id)
 
       assert response == expected_response
     end
