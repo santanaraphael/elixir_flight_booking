f8c436e8282fb988408afb1a479f71b31e53435e
Raphael Queiroz <uroboros.phael@gmail.com>
2021-07-25 21:34:44 -0300

    feat: add report by period.

--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -11,6 +11,14 @@ defmodule Flightex.Bookings.Report do
     |> then(fn line -> File.write!(filename, line) end)
   end
 
+  def generate_by_period(filename, start_date, end_date) do
+    BookingAgent.list_all()
+    |> Map.values()
+    |> Enum.filter(fn booking -> is_booking_in_period?(booking, start_date, end_date) end)
+    |> Enum.map(&generate_booking_line/1)
+    |> then(fn line -> File.write!(filename, line) end)
+  end
+
   defp generate_booking_line(%Booking{
          user_id: user_id,
          local_origin: local_origin,
@@ -19,5 +27,10 @@ defmodule Flightex.Bookings.Report do
        }) do
     {:ok, user} = UserAgent.get(user_id)
     "#{user.cpf},#{local_origin},#{local_destination},#{complete_date}\n"
+  end
+
+  defp is_booking_in_period?(%Booking{complete_date: complete_date}, start_date, end_date) do
+    NaiveDateTime.compare(complete_date, start_date) == :gt and
+      NaiveDateTime.compare(complete_date, end_date) == :lt
   end
 end
--- test/bookings/report_test.exs
+++ test/bookings/report_test.exs
@@ -1,7 +1,7 @@
 defmodule Flightex.Bookings.ReportTest do
   use ExUnit.Case, async: false
 
-  alias Flightex.Bookings.Report
+  alias Flightex.Bookings.{Report, Booking}
 
   import Flightex.Factory
 
@@ -11,7 +11,7 @@ defmodule Flightex.Bookings.ReportTest do
       :ok
     end
 
-    test "when called, return the content" do
+    test "when called, persist the content" do
       {:ok, user} =
         :user
         |> build()
@@ -26,7 +26,49 @@ defmodule Flightex.Bookings.ReportTest do
       Report.generate("report-test.csv")
       {:ok, file} = File.read("report-test.csv")
 
-      assert file =~ content
+      assert file == content
+    end
+  end
+
+  describe "generate_by_period/3" do
+    setup do
+      Flightex.start_agents()
+      :ok
+    end
+
+    test "when called, persist the content based on the period" do
+      {:ok, user} =
+        :user
+        |> build()
+        |> Flightex.create_or_update_user()
+
+      bookings =
+        build_list(5, :booking_input, %{user_id: user.id})
+        |> Enum.map(&Booking.build/1)
+        |> Enum.map(fn {:ok, %Booking{} = booking} -> booking end)
+
+      Enum.map(bookings, &Flightex.create_or_update_booking/1)
+
+      :booking
+      |> build(user_id: user.id, complete_date: ~N[2002-05-07 03:05:00])
+      |> Flightex.create_or_update_booking()
+
+      content =
+        "12345678900,Brasilia,Bananeiras,2001-05-07 03:05:00\n" <>
+          "12345678900,Brasilia,Bananeiras,2001-05-07 03:05:00\n" <>
+          "12345678900,Brasilia,Bananeiras,2001-05-07 03:05:00\n" <>
+          "12345678900,Brasilia,Bananeiras,2001-05-07 03:05:00\n" <>
+          "12345678900,Brasilia,Bananeiras,2001-05-07 03:05:00\n"
+
+      Report.generate_by_period(
+        "report-test.csv",
+        ~N[2001-05-02 03:05:00],
+        ~N[2001-05-20 03:05:00]
+      )
+
+      {:ok, file} = File.read("report-test.csv")
+
+      assert file == content
     end
   end
 end
